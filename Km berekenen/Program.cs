20d2c4ab3f579808922a370aacc62001b0fe0413
JARNO MAES <JARNOM@cursisten.intecbrussel.intra>
2020-10-29 15:36:39 +0100

    Add project files.

---
+++ Km berekenen/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Km_berekenen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double kmStart;
+            //double kmStop;
+            //double kmAfgelegd;
+            //double bijGetankt;
+            //double verbruikPer100km;
+            //double x;
+
+
+
+            //Console.WriteLine("Geef Start Km");
+            //kmStart = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Geef Eind Kilometers");
+            //kmStop = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Geef liter bijgetankt");
+            //bijGetankt = Convert.ToDouble(Console.ReadLine());
+
+
+            //kmAfgelegd = kmStop - kmStart;
+            //x = 100 / kmAfgelegd;
+            //verbruikPer100km = bijGetankt * x;
+            //Console.WriteLine(x);
+
+
+            double Getal;
+
+            Console.WriteLine("Enter Number");
+            Getal = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Getal + 15);
+            Console.ReadLine();
+            Console.WriteLine(Getal * 2);
+            Console.ReadLine();
+            Console.WriteLine(Getal - 4);
+            Console.ReadLine();
+            Console.WriteLine(Getal / 2);
+            Console.ReadLine();
+            
+            
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ StringDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string persoon = "Jarno";
+            //Console.WriteLine(persoon[2]);
+            //Console.WriteLine(persoon.Length);
+            //Console.WriteLine(persoon[persoon.Length-4]);
+
+            string naam = "Jarno";
+            int compared = string.Compare(naam, "Jarno");
+            Console.WriteLine(compared);
+
+
+            //string seperator = "   ";
+            //string stringName = "Jarno";
+            //string StringTwo = "Intec";
+            //string Title = stringName + seperator + StringTwo;
+            //Console.WriteLine(Title);
+            //Console.WriteLine($"{stringName} {seperator} {StringTwo}");
+
+
+            //string name = "Steven";
+            ////name = name + " Pitteman";
+            //name += " Pitteman";
+            //Console.WriteLine(name);
+
+
+            //string str = "\"Intec Brussel\t\t Rouppe Plein 16, 1000 Brussel\"";
+            //Console.WriteLine(str.Length);
+            //Console.WriteLine(str.ToUpper());
+            //Console.WriteLine(str.ToLower());
+            //Console.WriteLine(str.StartsWith("Int"));
+            //Console.WriteLine(str.EndsWith("el"));
+            //Console.WriteLine(str.Substring(6,15));
+            //Console.WriteLine(str.Remove(15,10));
+            //Console.WriteLine(str.Replace("Brussel", "Gent"));
+            //Console.WriteLine(str.Trim());
+            //Escape charachters
+
+            //Console.WriteLine(str);
+
+            //string Location = @"C:\Users\JARNOM\source\repos";
+            //Console.WriteLine(Location);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
